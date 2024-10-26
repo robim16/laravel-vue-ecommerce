b174e0242144c7c0e45a1d7df53c30dbc9b5bac4
robim16 <luis.hoyos@dydsoluciones.net>
2024-10-26 10:14:16 -0500

    agregar status on customer registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -54,6 +54,7 @@ class RegisteredUserController extends Controller
         $customer->user_id = $user->id;
         $customer->first_name = $names[0];
         $customer->last_name = $names[1] ?? '';
+        $customer->status = 'active';
         $customer->save();
 
         Auth::login($user);
